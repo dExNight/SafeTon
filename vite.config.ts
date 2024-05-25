e35e18cebe459b2874bde4bd19eb1b23157eea92
am.gubaidullin <am.gubaidullin@innopolis.university>
2024-05-25 15:57:32 +0300

    My bad

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/ ?
 export default defineConfig({
   plugins: [vue(), nodePolyfills()],
   resolve: {
