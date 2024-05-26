1e0fe680e425180647d1881b8befb9c5275a273a
am.gubaidullin <am.gubaidullin@innopolis.university>
2024-05-26 10:31:49 +0300

    Adaptive for thin devices now

--- src/components/Nft.vue
+++ src/components/Nft.vue
@@ -19,7 +19,7 @@
   background-color: rgba(51, 51, 51, 0.5); /* 50% прозрачности */
   border-radius: 20px;
   overflow: auto;
-  
+
   width: 180px;
   height: 245px;
 
@@ -59,6 +59,9 @@ p {
 
     min-width: 160px;
     min-height: 215px;
+
+    font-size: 16px;
+    line-height: 30px;
   }
 
   .suspicion {
@@ -66,6 +69,46 @@ p {
     font-weight: 600;
     font-size: 16px;
     line-height: 30px;
+  }
+}
+
+@media (max-width: 362px) {
+  .nft-elem {
+    width: 140px;
+    height: 185px;
+
+    min-width: 140px;
+    min-height: 185px;
+
+    font-size: 13px;
+    line-height: 26px;
+  }
+
+  .suspicion {
+    color: red;
+    font-weight: 600;
+    font-size: 13px;
+    line-height: 26px;
+  }
+}
+
+@media (max-width: 321px) {
+  .nft-elem {
+    width: 110px;
+    height: 155px;
+
+    min-width: 110px;
+    min-height: 155px;
+
+    font-size: 10px;
+    line-height: 22px;
+  }
+
+  .suspicion {
+    color: red;
+    font-weight: 600;
+    font-size: 10px;
+    line-height: 22px;
   }
 }
 </style>
